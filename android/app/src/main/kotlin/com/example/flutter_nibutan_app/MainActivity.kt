5b3ddf02fa227b36218809d0f7344dff7230b74e
dicenull <dicenull@users.noreply.github.com>
2022-07-28 14:30:24 +0000

    deploy: 44bfa642897f5b05cca0fab7aa0316a469e8fe1f

--- android/app/src/main/kotlin/com/example/flutter_nibutan_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_nibutan_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/nibutan_controller.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_nibutan_app/models/nibutan_state.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final nibutanProvider = StateProvider<AsyncValue<NibutanState>>(
-  (_) => const AsyncValue.loading(),
-);
